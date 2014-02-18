815a028da564a19d77224109fc93685eae05f0d4
Natalia Lehmann <natlehmann@gmail.com>
2014-02-18 19:54:29 -0300

    cambios de estilo de marce

---
+++ contacto.php
@@ -0,0 +1,22 @@
+
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'marcelaueno@hotmail.com';
+$titulo = 'ASUNTO DEL MENSAJE';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['Submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.');
+window.location.href = 'http://www.marcelaueno.com.ar/website_vickyvictoria_v5/contacto1.html';
+</script>";
+} else {
+echo 'Falló el envio';
+}
+}
+?>
+
