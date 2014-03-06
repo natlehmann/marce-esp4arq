6cac2da8c5057d0e0d5bb90355ad128f0c73ec7c
Natalia Lehmann <natlehmann@gmail.com>
2014-03-06 19:37:12 -0300

    se eliminan sliders de la home y servicios para version mobile

--- js/ajustesMobileHome.js
+++ js/ajustesMobileHome.js
@@ -1,32 +1,6 @@
 $( window ).load(function() {
 	
-	if ($(window).width() <= 568) {
-		
-		$("#contenidoHome").css("height", $(window).height() + "px");
-		
-		$("#slideShowImages img").hide();
-		
-//		$("#slideShowImages img").css("width", $(window).width() + "px");
-//		$("#slideShowImages img").css("height", "auto");
-//		$("#slideShowImages img").css("top", "-5000px");
-
-		initSlider( 1, 1, function(){
-			
-			$(".slidesjs-control").css("width", $(window).width() + "px");
-    		$(".slidesjs-control").css("height", "180px");
-    		
-    		$(".slidesjs-container").css("width", $(window).width() + "px");
-    		$(".slidesjs-container").css("height", "180px");
-    		
-    		$("#slideShowImages").css("margin-top", "0px");
-    		
-    		var altoItems = ($(window).height() - 60 - 180) / 6;
-    		$(".menuHomeMobile .menu-item").css("height", altoItems + "px");
-    		$(".menuHomeMobile .menu-item a").css("line-height", altoItems + "px");
-		} );
-	
-	
-	} else {
+	if ($(window).width() > 568) {
 		
 		initSlider( $("#contenidoHome").width(), $("#contenidoHome").height(), function(number) {
 			// Passes start slide number
